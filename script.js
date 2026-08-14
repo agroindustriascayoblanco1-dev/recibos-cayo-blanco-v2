c7ec586e4154e65551c6c6ddd71108c48092949e
agroindustriascayoblanco1-dev <agroindustriascayoblanco1@gmail.com>
2026-08-14 15:33:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 const PERIODOS = {
     q1: {
         nombre: "Quincena 1",
-        mes: "Julio 2026",
-        periodoPago: "1 al 15 de julio de 2026",
+        mes: "Agosto 2026",
+        periodoPago: "11 de julio al 25 de julio de 2026",
         archivo: "RECIBOS QUINCENA 1.pdf"
     },
     q2: {
         nombre: "Quincena 2",
         mes: "Julio 2026",
-        periodoPago: "16 al 31 de julio de 2026",
+        periodoPago: "26 de junio al 10 de julio de 2026",
         archivo: "RECIBOS QUINCENA 2.pdf"
     }
 };
